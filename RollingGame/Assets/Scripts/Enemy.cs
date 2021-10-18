29c939feaea3e5b307fd82cffa2069641129a311
egemen6k <egemen.altikardesler@gmail.com>
2021-10-18 20:11:15 +0300

    +Enemy ball chasing

---
+++ RollingGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3f;
+    private Rigidbody _rb;
+    private GameObject player;
+    private Vector3 _lookDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _lookDirection = (player.transform.position - transform.position).normalized;
+        _rb.AddForce(_lookDirection * speed);
+    }
+}
