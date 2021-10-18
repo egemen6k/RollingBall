8607b5ad643397d06848560ad921131b06bc048b
egemen6k <egemen.altikardesler@gmail.com>
2021-10-18 20:01:14 +0300

    +Player & Camera controller

---
+++ RollingGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody _rb;
+    private GameObject focalPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        _rb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}
